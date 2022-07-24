09beac703c8e90601db90187760fe141dfff2233
SaiGulipalli <venkatsaigulipalli@gmail.com>
2022-07-24 19:44:52 +0530

    Welcome

--- Welcome_Program.cpp
+++ Welcome_Program.cpp
@@ -1,10 +1,21 @@
-#include <iostream>
-using namespace std;
+#include <iostream> //Header file
+using namespace std; //Namespacee
 
-int main()
+int main()  // main function
 
-{
+{            //Everything which we write in this semicolons is known as block (LOGICALLY CONNECTED STATEMENTS)
 
     cout << "Hey Welcome !!";
     return 0;
 }
+
+
+// #include <iostream> 
+
+// int main()
+
+// {
+
+//     std::cout << "Hey Welcome !!";
+//     return 0;
+// }
